b6bab290a0ad871bc4b2e5fd027d768590da81ae
Harsh-0986 <harsh.r.shah2102@gmail.com>
2021-05-01 12:48:21 +0000

    Initail commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import TextEditor from './TextEditor'
 
+
 function App() {
   return <TextEditor />   
 }
--- client/src/TextEditor.js
+++ client/src/TextEditor.js
@@ -1,11 +1,16 @@
-import React from 'react'
+import { useCallback } from 'react'
 import Quill from 'quill'
 import 'quill/dist/quill.snow.css'
 
 export default function TextEditor() {
-    return (
-        <div>
-            Text Editor
-        </div>
-    )
+    const wrapperRef = useCallback((wrapper) =>{
+        if (wrapper == null) return
+
+        wrapper.innerHTML = ""
+        const editor = document.createElement('div')
+        wrapper.append(editor)
+        new Quill(editor, {theme: "snow"})
+    }, [])
+
+    return <div className="container" ref={wrapperRef}></div>
 }

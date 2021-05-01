f058b6fcd34a710ddb9d891e6a338ddd24401087
Harsh-0986 <harsh.r.shah2102@gmail.com>
2021-05-01 11:31:58 +0000

    Initial commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,7 @@
+import TextEditor from './TextEditor'
+
 function App() {
-  return "HI"    
+  return <TextEditor />   
 }
 
 export default App;
---
+++ client/src/TextEditor.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Quill from 'quill'
+import 'quill/dist/quill.snow.css'
+
+export default function TextEditor() {
+    return (
+        <div>
+            Text Editor
+        </div>
+    )
+}
